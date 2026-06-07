416ffb50c312a0d3dd35342861a26bcd00a1b73b
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-07 21:56:05 +0530

    Add portfolio code

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2 className="section-title">
+        About <span className="gradient-text">Me</span>
+      </h2>
+      <div className="about-content">
+        <p>
+          I'm a passionate Full Stack Developer with expertise in building
+          scalable web applications.
+        </p>
+        <p>
+          When I'm not coding, you'll find me exploring new technologies and
+          contributing to open-source projects.
+        </p>
+      </div>
+    </section>
+  );
+}
+export default About;
